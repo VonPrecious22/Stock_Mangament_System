d1ff24adacf7f9ee7cfc016f0c8d7a25a54affde
VonPrecious22 <atudprecious2@gmail.com>
2026-09-10 18:16:46 +0100

    connecting MongoDB

--- src/config/database.js
+++ src/config/database.js
@@ -1,9 +1,11 @@
 const mongoose = require("mongoose");
+
 require("dotenv").config();
 
 async function connectDatabase() {
   const databaseUrl =
     process.env.MONGODB || "mongodb://localhost:27017/stockmanagement";
+
   try {
     await mongoose.connect(databaseUrl);
     console.log("Connected to the database");
@@ -11,7 +13,6 @@ async function connectDatabase() {
     console.error("Database failed to connect", err);
     process.exit(1);
   }
-}`65rdkl`
+}
 
-// Export the function directly
 module.exports = connectDatabase;
